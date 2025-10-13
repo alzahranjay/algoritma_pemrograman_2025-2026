ee96f59a91cd32561d2b7346f93abd5a3b45e0b9
awew <alzahrapramana@gmail.com>
2025-10-13 10:52:53 +0700

    first commit

---
+++ Java/main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[]args) {
+        String nama ="Alzahra Cahya";
+        int umur = 18;
+        double tinggi = 159;
+        boolean aktif = true;
+
+        system.out.ptint1
+    }
+}
