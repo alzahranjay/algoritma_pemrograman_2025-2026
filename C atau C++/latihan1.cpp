afd942308626ef9a2bbcae5f2a59aea25e6a4ac5
awew <alzahrapramana@gmail.com>
2025-10-13 10:40:36 +0700

    latihan
    menambahkan code

---
+++ C atau C++/latihan1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string nama ="Alzahra";
+    int umur =18;
+    float tinggi =159;
+    bool aktif =true;
+
+    cout<<"nama:"<<nama<<endl;
+    cout<<"tiggi:"<<tinggi<<endl
+    cout<<"status aktif:"<<aktif<<endl;
+    return 0;
+}
---
+++ Java/Innercoba1.java
@@ -0,0 +1,13 @@
+class coba1 { 
+    public static void main(String[] args) { 
+        String nim = "I.2510091"; 
+        String nama = "Alzahra Cahya"; 
+        String umur = "18"; 
+        String profesi = "Mahasiswa"; 
+ 
+        System.out.println(nim); 
+        System.out.println(nama); 
+        System.out.println(umur); 
+        System.out.println(profesi); 
+    } 
+} 
